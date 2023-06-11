a9c29a13118ef934edf03d76c47a0be03f61841e
cssxsh <cssxsh@gmail.com>
2023-06-11 14:21:17 +0800

    feat: UploadConsoleRuntime

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,4 +52,44 @@ tasks {
     test {
         useJUnitPlatform()
     }
+    register("copyConsoleRuntime") {
+        group = "mirai"
+
+        val folder = file("run/console")
+        val libs = folder.resolve("libs")
+        libs.mkdirs()
+        val plugins = folder.resolve("plugins")
+        plugins.mkdirs()
+
+        doLast {
+            configurations.getByName("testConsoleRuntime").resolve().forEach { lib ->
+                val dest = libs.resolve(lib.name)
+                dest.delete()
+                lib.copyTo(dest, true)
+            }
+            buildDir.resolve("mirai/fix-protocol-version-${version}.mirai2.jar").let { plugin ->
+                val dest = plugins.resolve(plugin.name)
+                dest.delete()
+                plugin.copyTo(dest, true)
+            }
+            uri("https://raw.githubusercontent.com/RomiChan/protocol-versions/master/android_pad.json").toURL().let {
+                folder.resolve("android_pad.json").writeBytes(it.readBytes())
+            }
+            """java -D"file.encoding=utf-8" -cp "./libs/*" net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader""".let {
+                folder.resolve("start.cmd").writeText(it)
+                folder.resolve("start.sh").writeText(it)
+            }
+            folder.resolve("README.txt").writeText("""
+                登陆协议选 ANDROID_PAD
+                例如
+                login 123456 password ANDROID_PAD
+                
+                原理是回退到风控较轻的 旧版本协议 8.8.88，顺便这个 8.8.88 的信息实际上是 ANDROID_PHONE 的，所以会顶号
+                
+                这是独立的开发版本整合包，不能替换 mcl 的文件来使用
+                
+                整合包 by https://github.com/cssxsh
+            """.trimIndent())
+        }
+    }
 }
