cda1fc8d8291fe8f2cff059725d6cecdd73f4d90
cssxsh <cssxsh@gmail.com>
2023-06-21 19:02:15 +0800

    feat: command load

--- src/main/kotlin/xyz/cssxsh/mirai/tool/FixProtocolVersionCommand.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/tool/FixProtocolVersionCommand.kt
@@ -21,6 +21,17 @@ internal object FixProtocolVersionCommand : CompositeCommand(
     }
 
     @SubCommand
+    suspend fun CommandSender.load(protocol: BotConfiguration.MiraiProtocol) {
+        try {
+            FixProtocolVersion.load(protocol)
+            sendMessage(FixProtocolVersion.info()[protocol] ?: "找不到协议信息")
+        } catch (cause: Throwable) {
+            FixProtocolVersionPlugin.logger.warning(cause)
+            sendMessage("出现错误")
+        }
+    }
+
+    @SubCommand
     suspend fun CommandSender.info() {
         sendMessage(buildString {
             appendLine("当前各协议版本日期: ")
