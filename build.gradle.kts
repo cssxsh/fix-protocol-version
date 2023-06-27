e2f944a3e5f187e0acd906a6cb89f786bed8e60e
cssxsh <cssxsh@gmail.com>
2023-06-28 01:40:21 +0800

    feat: protocol in http

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,12 +28,18 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     //
-    implementation(platform("net.mamoe:mirai-bom:2.15.0-RC"))
+    implementation(platform("net.mamoe:mirai-bom:2.99.0-local"))
     compileOnly("net.mamoe:mirai-core")
     compileOnly("net.mamoe:mirai-core-utils")
     compileOnly("net.mamoe:mirai-console-compiler-common")
     testImplementation("net.mamoe:mirai-core-mock")
     testImplementation("net.mamoe:mirai-logging-slf4j")
+    //
+    implementation(platform("io.ktor:ktor-bom:2.2.4"))
+    implementation("io.ktor:ktor-client-okhttp")
+    implementation("io.ktor:ktor-client-encoding")
+    implementation("io.ktor:ktor-client-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
     //
     implementation(platform("org.slf4j:slf4j-parent:2.0.7"))
     testImplementation("org.slf4j:slf4j-simple")
--- src/main/kotlin/xyz/cssxsh/mirai/tool/TLV544Provider.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/tool/TLV544Provider.kt
@@ -1,14 +1,22 @@
 package xyz.cssxsh.mirai.tool
 
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.compression.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.future.*
 import kotlinx.serialization.json.*
-import net.mamoe.mirai.*
 import net.mamoe.mirai.internal.spi.*
 import net.mamoe.mirai.internal.utils.*
 import net.mamoe.mirai.utils.*
-import java.io.File
-import java.util.*
+import kotlin.coroutines.*
 
-public class TLV544Provider : EncryptService {
+public class TLV544Provider : EncryptService, CoroutineScope {
     internal companion object {
         val SALT_V1 = arrayOf("810_2", "810_7", "810_24", "810_25")
         val SALT_V2 = arrayOf("810_9", "810_a", "810_d", "810_f")
@@ -16,71 +24,89 @@ public class TLV544Provider : EncryptService {
 
         @JvmStatic
         internal val logger: MiraiLogger = MiraiLogger.Factory.create(TLV544Provider::class)
+    }
 
-        @JvmStatic
-        internal external fun sign(payload: ByteArray): ByteArray
-
-        init {
-            val os = when (val name = System.getProperty("os.name")) {
-                "Mac OS X" -> "macos"
-                "Linux" -> if (System.getenv("TERMUX_VERSION") != null) "android" else "linux"
-                else -> when {
-                    name.startsWith("Win") -> "windows"
-                    "The Android Project" == System.getProperty("java.specification.vendor") -> "android"
-                    else -> throw RuntimeException("Unknown OS $name")
-                }
-            }
-            val arch = when (val name = System.getProperty("os.arch")) {
-                "x86" -> "x86"
-                "x86_64", "amd64" -> "x64"
-                "aarch64" -> "arm64"
-                else -> throw RuntimeException("Unknown arch $name")
-            }
-            val filename = System.mapLibraryName("t544-enc-${os}-${arch}")
-            val file = File(System.getProperty("xyz.cssxsh.mirai.tool.t544", filename))
-            if (file.isFile.not()) {
-                this::class.java.getResource(filename)?.let { resource ->
-                    file.writeBytes(resource.readBytes())
-                } ?: kotlin.run {
-                    logger.error("not found: $filename")
-                }
-            } else {
-                this::class.java.getResource("$filename.sha256")?.let { sha256 ->
-                    val hash = sha256.readText().trim()
-                    val digest = java.security.MessageDigest.getInstance("SHA256")
-                    val now = digest.digest(file.readBytes()).toUHexString("").lowercase()
-                    if (hash != now) {
-                        logger.warning("SHA256 not match $hash with ${file.toPath().toUri()}")
-                    }
-                } ?: kotlin.run {
-                    logger.error("not found: $filename.sha256")
-                }
-            }
-            logger.info("load: ${file.toPath().toUri()}")
-            System.load(file.absolutePath)
+    @PublishedApi
+    internal val http: HttpClient = HttpClient(OkHttp) {
+        BrowserUserAgent()
+        ContentEncoding()
+        expectSuccess = true
+        install(HttpTimeout) {
+            socketTimeoutMillis = 30_000
+            connectTimeoutMillis = 30_000
+            requestTimeoutMillis = null
+        }
+        install(ContentNegotiation) {
+            json()
         }
     }
 
-    @Suppress("INVISIBLE_MEMBER")
+    override val coroutineContext: CoroutineContext = CoroutineExceptionHandler { context, exception ->
+        when (exception) {
+            is CancellationException -> {
+                // ...
+            }
+            else -> {
+                logger.warning({ "TLV544Provider with ${context[CoroutineName]}" }, exception)
+            }
+        }
+    }
+
+    @OptIn(MiraiInternalApi::class)
+    @Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
     override fun encryptTlv(context: EncryptServiceContext, tlvType: Int, payload: ByteArray): ByteArray? {
         if (tlvType != 0x544) return null
         val command = context.extraArgs[EncryptServiceContext.KEY_COMMAND_STR]
-        val protocol = try {
-            context.extraArgs[EncryptServiceContext.KEY_BOT_PROTOCOL]
-        } catch (_: NoSuchElementException) {
-            Bot.getInstanceOrNull(context.id)?.configuration?.protocol
-        }
+        val protocol = context.extraArgs[EncryptServiceContext.KEY_BOT_PROTOCOL]
+        val device = context.extraArgs[EncryptServiceContext.KEY_DEVICE_INFO]
 
         logger.info("t544 command: $command with $protocol")
 
-        return when (command) {
-            in SALT_V2 -> {
-                // from MiraiGo
-                sign(payload.copyInto(ByteArray(payload.size) { 0 }, 4, 4))
-            }
-            else -> {
-                sign(payload)
-            }
-        }
+        val future = async(CoroutineName("encryptTlv(${context.id})")) {
+            val impl = MiraiProtocolInternal.protocols[protocol]!!
+            val json = http.get("http://127.0.0.1:8080/energy") {
+                parameter("version", impl.sdkVer)
+                parameter("uin", context.id)
+                parameter("guid", device.guid.toUHexString(""))
+                parameter("data", command)
+            }.body<JsonObject>()
+
+            val data = checkNotNull(json["data"]?.jsonPrimitive?.content) { json.toString() }
+
+            data.hexToBytes()
+        }.asCompletableFuture()
+        return future.get()
+    }
+
+    override fun initialize(context: EncryptServiceContext) {
+        // TODO
+    }
+
+    override fun qSecurityGetSign(
+        context: EncryptServiceContext,
+        sequenceId: Int,
+        commandName: String,
+        payload: ByteArray
+    ): EncryptService.SignResult? {
+        val qua = context.extraArgs[EncryptServiceContext.KEY_APP_QUA]
+        val future = async(CoroutineName("qSecurityGetSign(${context.id})")) {
+            val json = http.get("http://127.0.0.1:8080/sign") {
+                parameter("uin", context.id)
+                parameter("qua", qua)
+                parameter("cmd", commandName)
+                parameter("seq", sequenceId)
+                parameter("buffer", payload.toUHexString(""))
+            }.body<JsonObject>()
+
+            val data = checkNotNull(json["data"]?.jsonObject) { json.toString() }
+
+            EncryptService.SignResult(
+                sign = data["sign"]!!.jsonPrimitive.content.hexToBytes(),
+                token = data["token"]!!.jsonPrimitive.content.hexToBytes(),
+                extra = data["extra"]!!.jsonPrimitive.content.hexToBytes()
+            )
+        }.asCompletableFuture()
+
+        return future.get()
     }
 }
