b5859e1dfac8c08cf49f8b604fbef21c0965e791
cssxsh <cssxsh@gmail.com>
2023-08-05 04:55:03 +0800

    fix: KFCFactoryTest

--- src/test/kotlin/xyz/cssxsh/mirai/tool/KFCFactoryTest.kt
+++ src/test/kotlin/xyz/cssxsh/mirai/tool/KFCFactoryTest.kt
@@ -7,10 +7,6 @@ import kotlin.test.*
 
 internal class KFCFactoryTest {
 
-    init {
-        System.setProperty(KFCFactory.CONFIG_PATH_PROPERTY, "example/KFCFactory.json")
-    }
-
     @Test
     fun service() {
         @Suppress("INVISIBLE_MEMBER")
